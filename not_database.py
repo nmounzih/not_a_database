4a54db29bdb6ae023bc306f85e1420126044715d
Nadia Mounzih <nmounzih@yahoo.com>
2017-03-08 10:44:34 -0500

    Added changes that we made in class

--- not_database.py
+++ not_database.py
@@ -10,7 +10,7 @@ class User:
         self.destination = destination
 
     def __repr__(self):
-        return self.user_name, self.password, self.full_name, self.destination
+        return "\nusername: {}\n password: {}\n name: {}\n destination: {}\n".format(self.user_name, self.password, self.full_name, self.destination)
 
     def see_info(self):
         print(self)
@@ -20,22 +20,12 @@ class User:
              csvfile.write("{},{},{},{}\n".format(self.user_name, self.password, self.full_name, self.destination))
         csvfile.close()
 
+    def is_valid_credentials(self, username, password):
+        return self.user_name == username and self.password == password
+
+
 def clear():
     os.system("clear")
-
-def show_user_info(login_name, login_password):
-    current_users = []
-    yes_user = []
-    with open("travel_plans.csv", 'r', newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        for row in reader:
-            current_users.append(row)
-    for info_list in current_users:
-        if info_list[0] == login_name and info_list[1] == login_password:
-            yes_user.append(info_list)
-    print("\nHere is your info: ")
-    for single_list in yes_user:
-        print("\nusername: {}\n password: {}\n name: {}\n destination: {}\n".format(single_list[0], single_list[1], single_list[2], single_list[3]))
 
 def check_login(login_name, login_password):
     current_users = []
@@ -43,32 +33,50 @@ def check_login(login_name, login_password):
     with open("travel_plans.csv", 'r', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
         for row in reader:
-            current_users.append(row)
-    for info_list in current_users:
-        if info_list[0] == login_name and info_list[1] == login_password:
-            yes_user.append(info_list)
-    if yes_user:
-        return True
-    else:
-        return False
+            u = User(*row)
+            current_users.append(u)
+    for user in current_users:
+        if user.is_valid_credentials(login_name, login_password):
+            print("\nHere is your info: ")
+            print(user)
+            return user
+    return None
+
+
+#def check_login(login_name, login_password):
+    # current_users = []
+    # yes_user = []
+    # with open("travel_plans.csv", 'r', newline='') as csvfile:
+    #     reader = csv.reader(csvfile, delimiter=',')
+    #     for row in reader:
+    #         current_users.append(row)
+    # for info_list in current_users:
+    #     if info_list[0] == login_name and info_list[1] == login_password:
+    #         yes_user.append(info_list)
+    # if yes_user:
+    #     return True
+    # else:
+    #     return False
 
 
 def main():
-    success = 0
+    #success = 0
     print("\n*Welcome to Travel Info*\n")
     while True:
-        while success == 0:
+        current_user = None
+        while not current_user:
             login_name = input("Press [q] to quit. \n\nWhat is your username? ").lower()
             if login_name == 'q':
                 exit()
             login_password = input("Enter your password: ")
-            if check_login(login_name, login_password) is True:
+            current_user = check_login(login_name, login_password)
+            if current_user:
                 print("You have successfully logged in.")
-                success += 1
+                #success += 1
             else:
                 print("Incorrect username or password. Try again.") # make loop to let them try again
-        while success == 1:
-            show_user_info(login_name, login_password)
+        while current_user:
+            #show_user_info(login_name, login_password)
             user_choice = input("[C]reate new user or [L]og out? ").lower()
             if user_choice == 'c':
                 new_username = input("Please enter new username: ")
@@ -82,7 +90,8 @@ def main():
 
             elif user_choice == 'l':
                 clear()
-                success -= 1
+                current_user = None
+                #success -= 1
                 break
 
 main()
